ea75283ddb10f243decfc44c3350f0c48f9c0fd1
Jan Kiesewetter <jan@t3easy.de>
2016-01-19 23:03:21 +0100

    [TASK] Provide composer.json and raise version to 1.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.0.1',
+	'version' => '1.0.2',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.6-6.2.99',
+			'typo3' => '6.2.16-6.2.99',
 		),
 		'conflicts' => array(
 		),

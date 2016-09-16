31aa522ea6abd8ae9785a984c45ecd73e0bcbde0
Jan Kiesewetter <jan@t3easy.de>
2016-09-16 10:04:48 +0200

    [TASK] Update compatible TYPO3 versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '2.0.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.1-7.6.99',
+            'typo3' => '7.6.0-8.9.99',
         ),
         'conflicts' => array(
         ),

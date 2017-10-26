057ead817e7b806f7d4eb1e84c3a415794a12c3a
Olaf Hoffmann <o.hoffmann@seidemann-web.com>
2017-10-26 09:22:44 +0200

    Adds ext_localconf for correct XCLASSing

---
+++ ext_localconf.php
@@ -0,0 +1,14 @@
+<?php
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Extbase\\Domain\\Model\\FileReference'] = [
+    'className' => 'T3easy\\Faltranslation\\Domain\\Model\\FileReference'
+];
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Extbase\\Persistence\\Generic\\Mapper\\ColumnMap'] = [
+    'className' => 'T3easy\\Faltranslation\\Persistence\\Generic\\Mapper\\ColumnMap'
+];
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Extbase\\Persistence\\Generic\\Mapper\\DataMapFactory'] = [
+    'className' => 'T3easy\\Faltranslation\\Persistence\\Generic\\Mapper\\DataMapFactory'
+];
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Extbase\\Persistence\\Generic\\Mapper\\DataMapper'] = [
+    'className' => 'T3easy\\Faltranslation\\Persistence\\Generic\\Mapper\\DataMapper'
+];
